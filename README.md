fd49757159162841dff8faf96e3bfce05c4042d5
Martin Machacek <machacek@edhouse.cz>
2023-08-04 20:10:21 +0200

    Added script Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # graph-mailbox-user-purpose
-PowerShell script to read the purpose of the mailbox
+The repository contains the PowerShell script for reading the user purpose of the mailbox either for a specific user or for all users in the tenant.
+
+## Prerequisities
+- Register the app in Azure ADD with the following application permissions
+  - MailboxSettings.Read
+  - User.Read.All
+- Create a new client secret
+- Install the **Microsoft.Graph** PowerShell module
+
+## Examples
+### Read the user purpose of a specific user
+
+```
+Get-UserMailboxSettingUserPurpose `
+    -ClientId "<client_id>" `
+    -TenantId "<tenant_id>" `
+    -Secret "<client_secret>" `
+    -UserId "<user_id> or <service_principal_name>"
+```
+
+### Read the user purpose for all users in the tenant
+```
+Get-UsersMailboxSettingUserPurpose `
+    -ClientId "<client_id>" `
+    -TenantId "<tenant_id>" `
+    -Secret "<client_secret>" `
+```
---
+++ ReadMailboxSettings.ps1
@@ -0,0 +1,77 @@
+﻿# Get the user purpose of the mailbox for a specific user
+# Requires MailboxSettings.Read permission
+function Get-UserMailboxSettingUserPurpose {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory)]
+        [string] $ClientId,
+        [parameter(Mandatory)]
+        [string] $TenantId,
+        [parameter(Mandatory)]
+        [string] $Secret,
+        [parameter(Mandatory)]
+        [string] $UserId
+    )
+
+    $securedPasswordPassword = ConvertTo-SecureString `
+    -String $Secret -AsPlainText -Force
+
+    $clientSecretCredential = New-Object `
+    -TypeName System.Management.Automation.PSCredential `
+    -ArgumentList $ClientId, $securedPasswordPassword
+
+    Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $clientSecretCredential
+
+    Import-Module Microsoft.Graph.Users
+
+    $settings = Get-MgUserMailboxSetting -UserId $UserId -Property "userPurpose"
+    return $settings.UserPurpose
+}
+
+# Get the user purpose of the mailbox for all users in the tenant. Guest users are excluded
+# Requires User.Read.All MailboxSettings.Read permissions
+function Get-UsersMailboxSettingUserPurpose {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory)]
+        [string] $ClientId,
+        [parameter(Mandatory)]
+        [string] $TenantId,
+        [parameter(Mandatory)]
+        [string] $Secret
+    )
+
+    $securedPasswordPassword = ConvertTo-SecureString `
+    -String $Secret -AsPlainText -Force
+
+    $clientSecretCredential = New-Object `
+    -TypeName System.Management.Automation.PSCredential `
+    -ArgumentList $ClientId, $securedPasswordPassword
+
+    Connect-MgGraph -TenantId $TenantId -ClientSecretCredential $clientSecretCredential
+
+    Import-Module Microsoft.Graph.Users
+
+    $usersPurpose = @{}
+    $users = Get-MgUser `
+                -Property "id,displayName,userPrincipalName" `
+                -Filter "UserType ne 'Guest'" `
+                -ConsistencyLevel eventual `
+                -CountVariable CountVar `
+                | ForEach-Object {
+                    $settings = Get-MgUserMailboxSetting -UserId $_.Id -Property "userPurpose"
+                    $usersPurpose[$_.Id]=$settings.UserPurpose
+                }
+    return $usersPurpose
+} 
+
+Get-UserMailboxSettingUserPurpose `
+    -ClientId "..." `
+    -TenantId "..." `
+    -Secret "..." `
+    -UserId "..."
+
+Get-UsersMailboxSettingUserPurpose `
+    -ClientId "..." `
+    -TenantId "..." `
+    -Secret "..."
